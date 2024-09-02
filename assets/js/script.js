b37ff56997fc98467415cc54148558db67073d64
Kaniltazo <angeldanielrm@icloud.com>
2024-09-02 16:09:35 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,21 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+
+const items = document.querySelectorAll('.carousel-item');
+const container = document.querySelector('.carousel-container');
+
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        // Detener la animación del carrusel
+        container.style.animationPlayState = 'paused';
+
+        // Mover el ícono seleccionado al centro
+        items.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+
+        item.style.transform = 'translate(-50%, -50%) scale(1.5)';
+    });
+});
